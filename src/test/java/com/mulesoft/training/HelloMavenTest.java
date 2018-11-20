9f0995b3bfb729298a5f31a11ed4ecd939e10a02
Sathiyanarayana kumar <sathiyanarayana.kumar@int.whishworks.com>
2018-11-20 09:31:35 +0530

    Changes in maven-project.xml.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -7,7 +7,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven");
+        runFlowAndExpect("mavenHello", "Hello Maven");
     }
     
     @Override
